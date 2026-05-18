52b43952013197df02dc4babe115dd4dddd9e3af
Blaarg987 <powellaustin944@gmail.com>
2026-05-17 20:55:52 -0400

    Added a get request to return the entire devices list which contains LACP data about the LAGS. Also added some parsing logic to loop through it and extract some of the data from the members, to tired to write the entire thing right now and I am still learning (and struggling) with parsing JSON with for loops.....

--- main.py
+++ main.py
@@ -1,29 +1,51 @@
 import os
-
+import json
 import requests
 import urllib3
 from dotenv import load_dotenv
-
 load_dotenv()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 base_url = os.getenv("unifi_url")
 username = os.getenv("unifi_username")
-password = os.getenv("unifi_password") 
+password = os.getenv("unifi_password")
 
 
-auth = requests.post(
-    f"{base_url}/api/auth/login",
+def get_cookie():
+    auth = requests.post(
+        f"{base_url}/api/auth/login",
+        headers={"Content-Type": "application/json"},
+        json={"username": username, "password": password},
+        verify=False,
+        timeout=15,
+    )
+
+    if auth.status_code == 200:
+        print("Authenticated with UniFi controller.")
+        return auth.cookies
+    else:
+        print(f"Authentication failed. Status code: {auth.status_code}")
+        raise KeyError("Check your credentials and try again.")
+
+
+cookies = get_cookie()
+
+lacp_response = requests.get(
+    f"{base_url}/proxy/network/api/s/default/stat/device",
     headers={"Content-Type": "application/json"},
-    json={
-        "username": username,
-        "password": password,
-    },
+    cookies=cookies,
     verify=False,
     timeout=15,
 )
 
-print(f"Status: {auth.status_code}")
-print(auth.text)
-cookies = auth.cookies.get_dict()
-print(f"Cookies returned: {auth.cookies.get_dict()}")
+lacp_json = lacp_response.json()
+
+for device in lacp_json["data"]:
+    if device["type"] != "usw":
+        continue
+    for port in device["port_table"]:
+        if port["op_mode"] != "aggregate":
+           continue
+        for member in port["lacp_state"]:
+           print(member["member_port"], member["active"])
+            
